b75db29fe481e657abe5e0443530a7dab788276d
Alexander Brandes <brandes.alexander@web.de>
2023-05-01 10:12:17 +0200

    Address gradle deprecation

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,7 +109,7 @@ publishing {
 }
 
 nexusPublishing {
-    repositories {
+    this.repositories {
         sonatype {
             nexusUrl.set(URI.create("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(URI.create("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
